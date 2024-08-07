1285e681e6801954155d7655d051eff52379df6f
WibuSyariah <mspa43@gmail.com>
2024-08-07 13:35:04 +0700

    vue init, tailwind init, formkit init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { plugin, defaultConfig } from '@formkit/vue'
+import App from './App.vue'
+
+createApp(App).use(plugin, defaultConfig).mount('#app')
